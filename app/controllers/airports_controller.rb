9e4459874213f3a5d5f32dd505faac75489abece
bellamysmith <bellamysmith1@gmail.com>
2014-09-17 11:52:53 -0700

    changing front-page @airports

--- app/controllers/airports_controller.rb
+++ app/controllers/airports_controller.rb
@@ -4,8 +4,8 @@ class AirportsController < ApplicationController
 	include HTTParty
 
 	def index
-		@airports = Airport.all
-		#@airports = Airport.all.sort_by{ |air| air.reviews.length}.reverse.values_at(0,1,2,3,4)
+		#@airports = Airport.all
+		@airports = Airport.all.sort_by{ |air| air.reviews.length}.reverse.values_at(0,1,2,3,4)
 		
 		if params[:search]
   			@searches = Airport.search(params[:search])
